2322615a44ab428037a16eea08577923c9490957
Sihong Huang <sihongspring2011@gmail.com>
2018-12-04 14:24:51 -0500

    work on CatItemComponent

--- src/app/cat-list/cat-item/cat-item.component.ts
+++ src/app/cat-list/cat-item/cat-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Cat } from '../cat.model';
 
 @Component({
   selector: 'app-cat-item',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cat-item.component.scss']
 })
 export class CatItemComponent implements OnInit {
+  @Input() cat: Cat;
 
   constructor() { }
 
